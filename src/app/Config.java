76359f7f7366e74c4374fa2206ab1c0ad5955272
Metylene <metylene@gmx.fr>
2016-03-11 02:27:08 +0100

    Ajout d'un système de config de l'appli

---
+++ src/app/Config.java
@@ -0,0 +1,43 @@
+package app;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config{
+
+	private Properties properties;
+	private static Config _instance;
+
+	private Config(String filePath) throws IOException {
+		FileInputStream in = new FileInputStream(filePath);
+		properties.load(in);
+		in.close();
+	}
+	
+	/**
+	 * Renvoie l'instance unique de la classe Config
+	 * @param filePath ex : config.properties
+	 * @return Config
+	 */
+	public static Config getInstance(String filePath){
+		if(_instance == null){
+			try {
+				_instance = new Config(filePath);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return _instance;
+	}
+	
+	/**
+	 * Renvoie la propriété liée à la clé entrée en paramètre
+	 * @param key String
+	 * @return String
+	 */
+	public String getProperty(String key){
+		return properties.getProperty(key);
+	}
+
+}
